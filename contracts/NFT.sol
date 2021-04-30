ac42342b3766394a14fb7c7ab3b0dfdec4f894cb
ana0 <sarah.anne.friend@gmail.com>
2021-04-30 13:29:40 +0200

    Add Opensea recommendations for matic

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -1,12 +1,15 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.7.0;
+pragma solidity >=0.6.0 <0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./NativeMetaTransaction.sol";
+import "./ContextMixin.sol";
 
-contract NFT is Ownable, ERC721 {
+contract NFT is Ownable, ERC721, ContextMixin, NativeMetaTransaction {
 
     constructor (string memory name_, string memory symbol_) Ownable() ERC721(name_, symbol_) {
+        _initializeEIP712(name_);
     }
 
     function setBaseURI(string memory baseURI_) public onlyOwner {
@@ -16,5 +19,31 @@ contract NFT is Ownable, ERC721 {
     function mint(uint256 tokenId, string memory tokenURI) public onlyOwner {
         _safeMint(owner(), tokenId);
         _setTokenURI(tokenId, tokenURI);
+    }
+
+    /**
+     * This is used instead of msg.sender as transactions won't be sent by the original token owner, but by OpenSea.
+     */
+    function _msgSender()
+        internal
+        override
+        view
+        returns (address payable sender)
+    {
+        return ContextMixin.msgSender();
+    }
+
+    /**
+    * As another option for supporting trading without requiring meta transactions, override isApprovedForAll to whitelist OpenSea proxy accounts on Matic
+    */
+    function isApprovedForAll(
+        address _owner,
+        address _operator
+    ) public override view returns (bool isOperator) {
+        if (_operator == address(0x58807baD0B376efc12F5AD86aAc70E78ed67deaE)) {
+            return true;
+        }
+        
+        return ERC721.isApprovedForAll(_owner, _operator);
     }
 } 
--- truffle-config.js
+++ truffle-config.js
@@ -8,15 +8,15 @@ module.exports = {
      port: 8545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
     },
-    xdai: {
-      provider: function() {
-        return new HDWalletProvider(
+    matic: {
+      provider: () => new HDWalletProvider(
         process.env.MNEMONIC,
-        "https://dai.poa.network")
-      },
-      network_id: 100,
-      gas: 12487794,
-      gasPrice: 1000000000,
+        `https://rpc-mainnet.maticvigil.com`
+      ),
+      network_id: 137,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: false
     },
   },
 
@@ -28,7 +28,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.7.0",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: ">=0.6.0 <0.8.0",    // Fetch exact version from solc-bin (default: truffle's version)
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       // settings: {          // See the solidity docs for advice about optimization and evmVersion
       //  optimizer: {
