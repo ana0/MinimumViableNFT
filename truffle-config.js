c9525b726a49f3437f9c8bd5b193b5319c3c7764
ana0 <sarah.anne.friend@gmail.com>
2021-11-19 18:22:23 +0100

    Fix truffle-config formatting

--- truffle-config.js
+++ truffle-config.js
@@ -18,16 +18,6 @@ module.exports = {
       timeoutBlocks: 200,
       skipDryRun: false
     },
-  aurora: {
-    provider: () => new HDWalletProvider(
-      process.env.MNEMONIC,
-      `https://testnet.aurora.dev`,
-      0,
-      3,
-      true,
-    ),
-    network_id: 0x4e454153,
-    gas: 10000000,
   },
 
   // Set default mocha options here, use special reporters etc.
@@ -38,15 +28,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: ">=0.8.0",    // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
+      version: "^0.8.0"
     }
   },
   db: {
